7ec72c3a786ed5cdc7adf7a29341e21a3961dc47
Eileen <C16339313@mydit.ie>
2018-03-25 14:12:05 +0100

    good guys added nearly done!

---
+++ Test1/Assets/goodGuySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goodGuySpawner : MonoBehaviour {
+
+	public GameObject angle;
+
+	private float randY;
+	Vector2 wheretoSpawn;
+	public float spawnRate;
+	float nextSpawn=0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > nextSpawn) {
+			nextSpawn = Time.time + spawnRate;
+			randY = Random.Range (-2.4f,6.4f);
+			wheretoSpawn = new Vector2 (transform.position.x, randY);
+			Instantiate (angle, wheretoSpawn, Quaternion.identity);
+		}
+
+	}
+}
